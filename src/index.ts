ade4b03b0d0c71f901b79089e6bd93f87a7585f4
pulkitgangwar <pulkitgangwar2016@gmail.com>
2022-03-09 21:50:53 +0530

    added bad code example

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("hello from typescript");
+import fs from "fs";
+
+const matchesInCSV = fs.readFileSync("./football.csv", { encoding: "utf-8" });
+const matches = matchesInCSV
+  .split("\n")
+  .map((match: string): string[] => match.split(","));
+
+let manUnitedWins = 0;
+
+for (let match of matches) {
+  if (match[1] === "Man United" && match[5] === "H") {
+    manUnitedWins++;
+  } else if (match[2] === "Man United" && match[5] === "A") {
+    manUnitedWins++;
+  }
+}
+
+console.log(`man united won ${manUnitedWins} games`);
